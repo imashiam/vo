a4091c3b34a4ed469f4b8bf72909bb3861070bbd
TeleDALAD <ashkankorouei@gmail.com>
2016-03-09 14:59:10 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -228,29 +228,26 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {175623013,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
 
-https://github.com/SEEDTEAM/TeleSeed
 
 Admins
-@Empero [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
+@dlt_admin_2 [Founder]
+@dlt_admin_2 [Developer]
+@dlt_admin_2 [Developer]
+@dlt_admin_2 [Manager]
+@dlt_admin_2 [Admin]
 
 Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
+seed team
+abolfazl.k
+Dalton team
 
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@iran_DALAD [persian]
 ]],
     help_text_realm = [[
 Realm Commands:
