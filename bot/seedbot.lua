943df42648880a5424c794cbe9892bebd84882fa
imashiam <ashkanpj.ap2@gmail.com>
2016-03-11 03:11:28 +0330

    A H D

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -228,7 +228,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {175623013,0,tonumber(our_id)},--Sudo users
+    sudo_users = {81444630,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[ ]],
     help_text_realm = [[
